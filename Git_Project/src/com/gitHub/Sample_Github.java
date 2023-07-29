807791460d2bdc69518d4fd3b240fbe4aceb98af
Pavithran678 <139203236+Pavithran678@users.noreply.github.com>
2023-07-29 11:04:26 +0530

    Push Updated

--- Git_Project/src/com/gitHub/Sample_Github.java
+++ Git_Project/src/com/gitHub/Sample_Github.java
@@ -2,4 +2,11 @@ package com.gitHub;
 
 public class Sample_Github {
 
+  public void push(){
+    String s = "Pavithran";
+    System.out.println(s);
+}
+  public static void main(String [] args){
+    push();
+  }
 }
